0471bba24b7ee0265c9b561a84b7b37c8cc352b9
Nair <mnair@AffinionDS.Com>
2019-01-22 11:30:36 -0600

    Cleanup on code

--- restaurant-service/src/main/java/com/mt/restaurant/RestaurantApplication.java
+++ restaurant-service/src/main/java/com/mt/restaurant/RestaurantApplication.java
@@ -3,11 +3,11 @@ package com.mt.restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class RestaurantApplication {
 
 	public static void main(String[] args) {
--- roster-service/src/main/java/com/mt/roster/service/ResertvationController.java
+++ roster-service/src/main/java/com/mt/roster/service/ResertvationController.java
@@ -6,12 +6,16 @@ import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 
@@ -24,13 +28,15 @@ public class ResertvationController {
 
 	private Logger LOGGER = LoggerFactory.getLogger(ResertvationController.class);
 
- 
+	@Autowired
+    RestTemplate restTemplate;
 
 	@RequestMapping(path = "/{restaurant_id}", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<RestaurantVO> getDataTransfer(@PathVariable("restaurant_id") Integer id) {
 		RestaurantVO restaurant=null;
 		try {
-//			restaurant = service.findById(id);
+			ResponseEntity<RestaurantVO> responseEntity =restTemplate.exchange("http://RESTAURANT-SERVICE/restaurants/1", HttpMethod.GET, null,new ParameterizedTypeReference<RestaurantVO>() {});
+			restaurant= responseEntity.getBody();
 		} catch (Exception e) {
 			return new ResponseEntity<RestaurantVO>(HttpStatus.INTERNAL_SERVER_ERROR); 
 		}
